9480b727cb2b0073c465395ebd6705fa4f3f8b85
Wafa <enisowafa2014@homail.com>
2022-09-20 16:43:13 +0100

    230. Assignment Review: Handle Number Format Exception

---
+++ src/main/java/guru/springframework/controllers/ControllerAdvice.java
@@ -0,0 +1,46 @@
+package guru.springframework.controllers;
+
+import guru.springframework.exceptions.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.NoSuchElementException;
+
+@Slf4j
+@org.springframework.web.bind.annotation.ControllerAdvice
+public class ControllerAdvice {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, NoSuchElementException.class})
+    public ModelAndView handleNotFound(Exception exception){
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormatException(Exception exception){
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+
+
+        return modelAndView;
+    }
+}
